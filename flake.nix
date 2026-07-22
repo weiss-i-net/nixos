56a82e9ecbac524e42b3872bb69dff9de6c33595
Jannik Hiller <jannik.hiller@live.de>
2026-07-22 17:15:49 +0200

    Wire up nix fmt, pin transitive nixpkgs via follows, drop unused darwin systems

--- flake.nix
+++ flake.nix
@@ -5,9 +5,15 @@
     flake-parts.url = "github:hercules-ci/flake-parts";
     import-tree.url = "github:vic/import-tree";
 
-    wrapper-modules.url = "github:BirdeeHub/nix-wrapper-modules";
+    wrapper-modules = {
+      url = "github:BirdeeHub/nix-wrapper-modules";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
-    zen-browser.url = "github:0xc000022070/zen-browser-flake";
+    zen-browser = {
+      url = "github:0xc000022070/zen-browser-flake";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
     nixos-hardware = {
       url = "github:NixOS/nixos-hardware";
--- modules/parts.nix
+++ modules/parts.nix
@@ -2,9 +2,7 @@
   config = {
     systems = [
       "x86_64-linux"
-      "x86_64-darwin"
       "aarch64-linux"
-      "aarch64-darwin"
     ];
   };
 }
