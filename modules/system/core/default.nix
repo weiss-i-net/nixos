a4a11da814d0a4cf4b4278a48c87c923d70098d0
Jannik Hiller <jannik.hiller@live.de>
2026-07-31 12:02:00 +0200

    hosts: set hardware.enableRedistributableFirmware so CPU microcode updates actually apply

--- modules/system/core/default.nix
+++ modules/system/core/default.nix
@@ -23,6 +23,7 @@
       programs.fish.enable = true;
 
       nixpkgs.config.allowUnfree = true;
+      hardware.enableRedistributableFirmware = true;
       nix = {
         settings.experimental-features = [
           "nix-command"
